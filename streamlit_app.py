455a707dd0ff6782f272c5b193ef890aa8443c6c
Marek Cygan <cygan@mimuw.edu.pl>
2021-12-07 08:22:45 +0100

    Adding UW logo and reprasing text on the page.

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,14 @@ import streamlit as st
 import urllib
 import cv2
 
+MAX_IMG_WIDTH = 400
+MAX_IMG_HEIGHT = 200
+FILE_PATH='logo-uw.png'
+DEFAULT_IMAGE_URL = 'https://en.wikipedia.org/wiki/University_of_Warsaw#/media/File:POL_University_of_Warsaw_logo.svg'
+
+
 """
-# Welcome to Streamlit!
+# Affine transformations in OpenCV
 
 Use the sliders to modify the entries in a 2x3 matrix that defines an [affine transform in opencv](https://docs.opencv.org/3.4/d4/d61/tutorial_warp_affine.html) (which preserves lines parallelism).
 """
@@ -26,22 +32,20 @@ def read_file_from_url(url):
     return urllib.request.urlopen(url).read()
 
 
-MAX_IMG_WIDTH = 600
-MAX_IMG_HEIGHT = 400
-DEFAULT_IMAGE_URL = 'https://upload.wikimedia.org/wikipedia/commons/5/59/Brama_G%C5%82%C3%B3wna_kampus_centralny_Uniwersytetu_Warszawskiego_2019.jpg'
-
-
 file_obj = st.sidebar.file_uploader('Choose an image:', ('jpg', 'jpeg'))
+
+with open(FILE_PATH, 'rb') as f:
+    file_obj = BytesIO(f.read())
 
 if not file_obj:
     file_obj = BytesIO(read_file_from_url(DEFAULT_IMAGE_URL))
 
-img_in = PIL.Image.open(file_obj)
+img = PIL.Image.open(file_obj)
 
-img_in.thumbnail((MAX_IMG_WIDTH, MAX_IMG_HEIGHT), PIL.Image.ANTIALIAS)
-img_in = np.float32(img_in)
+img.thumbnail((MAX_IMG_WIDTH, MAX_IMG_HEIGHT), PIL.Image.ANTIALIAS)
+img = np.float32(img)
 
-rows,cols,channels = img_in.shape
+rows,cols,channels = img.shape
 
 M00 = st.sidebar.slider("M00", -2.0, 2., 1.)
 M01 = st.sidebar.slider("M01", -2.0, 2., 0.)
@@ -51,10 +55,19 @@ M10 = st.sidebar.slider("M10", -2.0, 2., 0.)
 M11 = st.sidebar.slider("M11", -2.0, 2., 1.)
 M12 = st.sidebar.slider("M12", -100, 100, 0)
 
+"""
+## Code snippet in OpenCV
+"""
+
 with st.echo(code_location='below'):
     M = np.float32([[M00, M01, M02],[M10, M11, M12]])
-    dst = cv2.warpAffine(img_in,M,(cols,rows))
+    dst = cv2.warpAffine(img, M, (cols,rows))
+
+
+"""
+## Matrix M
+"""
 
 st.write(M)
-write_image(st, img_in)
+write_image(st, img)
 write_image(st, dst)
